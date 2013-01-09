d34d852477f1a8cf8dcfbe9ebc40e62591680d62
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2013-01-09 21:37:20 +0200

    using specific environment variables

--- 2build_cpython_statically_nacl.sh
+++ 2build_cpython_statically_nacl.sh
@@ -3,9 +3,9 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPT_PATH=`dirname "$SCRIPT"`
 
-PLAT=${HOME}/nacl_sdk/pepper_23/toolchain/linux_x86_glibc
-ZRT_ROOT=${HOME}/git/zrt/lib
-LINK="-T ${PLAT}/x86_64-nacl/lib64/ldscripts/elf64_nacl.x.static -melf64_nacl -m64 -L${ZRT_ROOT} -lzrt -lfs -lstdc++ -lzglibc"
+PLAT=${NACL_SDK_ROOT}/toolchain/linux_x86_glibc
+ZRT_LIB=${ZRT_ROOT}/lib
+LINK="-T ${PLAT}/x86_64-nacl/lib64/ldscripts/elf64_nacl.x.static -melf64_nacl -m64 -L${ZRT_LIB} -lzrt -lfs -lstdc++ -lzglibc"
 
 export PATH=${PATH}:${PLAT}/bin
 echo $PATH
@@ -18,7 +18,7 @@ CXX="x86_64-naclg++" \
 AR="x86_64-nacl-ar" \
 RANLIB="x86_64-nacl-ranlib" \
 LD_LIBRARY_PATH=${PLAT}"/x86_64-nacl/lib" \
-CFLAGS="-D__NACL__ -I${ZRT_ROOT}" \
+CFLAGS="-D__NACL__ -I${ZRT_LIB}" \
 LDFLAGS=" -s -static -static-libgcc" \
 LIBC="-lzglibc" \
 ./configure --host=x86_64-nacl --build=x86_64-linux-gnu --prefix=/python --without-threads --enable-shared=no 
