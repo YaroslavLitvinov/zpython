28cefc205c4089c3219b8d619b69672a8df9af1c
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2013-01-14 14:34:15 +0200

    improved building of python

--- 2configure_zpython.sh
+++ 2configure_zpython.sh
@@ -4,23 +4,21 @@ NACL_TOOLCHAIN=${NACL_SDK_ROOT}/toolchain/linux_x86_glibc
 
 export PATH=${PATH}:${NACL_TOOLCHAIN}/bin
 
-LINK="-s -static -T \
-${NACL_TOOLCHAIN}/x86_64-nacl/lib64/ldscripts/elf64_nacl.x.static \
--melf64_nacl -m64 \
--L${ZRT_ROOT}/lib -lzrt -lfs -lstdc++ -lzglibc"
-
 #configure cpython to be built statically, also overrided LINKFORSHARED variable,
 #although rest variables has been set to link it statically by nacl-gcc
-PYTHONPATH="${ZPYTHON_ROOT}/Modules:$${ZPYTHON_ROOT}/Lib:${ZPYTHON_ROOT}" \
-LINKFORSHARED="${LINK}" \
-CC="x86_64-nacl-gcc" \
-CXX="x86_64-naclg++" \
-AR="x86_64-nacl-ar" \
-RANLIB="x86_64-nacl-ranlib" \
-LD_LIBRARY_PATH=${NACL_TOOLCHAIN}"/x86_64-nacl/lib" \
-CFLAGS="-I${ZRT_ROOT}/lib" \
-LDFLAGS=" -s -static -static-libgcc" \
-LIBC="-lzglibc" \
+export PYTHONPATH="${ZPYTHON_ROOT}/Modules:$${ZPYTHON_ROOT}/Lib:${ZPYTHON_ROOT}"
+export LINKFORSHARED=\
+"-s -static -T ${NACL_TOOLCHAIN}/x86_64-nacl/lib64/ldscripts/elf64_nacl.x.static \
+-melf64_nacl -m64 -L${ZRT_ROOT}/lib -lzrt -lfs -lstdc++ -lzglibc"
+export CC="x86_64-nacl-gcc"
+export CXX="x86_64-naclg++"
+export AR="x86_64-nacl-ar"
+export RANLIB="x86_64-nacl-ranlib"
+export LD_LIBRARY_PATH=${NACL_TOOLCHAIN}"/x86_64-nacl/lib"
+export CFLAGS="-I${ZRT_ROOT}/lib "
+export LDFLAGS=" -s -static -static-libgcc"
+export LIBC="-lzglibc"
+
 ./configure \
 --host=x86_64-nacl \
 --build=x86_64-linux-gnu \
--- 3build_zpython_statically.sh
+++ 3build_zpython_statically.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-PLAT=${NACL_SDK_ROOT}/toolchain/linux_x86_glibc
-export PATH=${PATH}:${PLAT}/bin
+NACL_TOOLCHAIN=${NACL_SDK_ROOT}/toolchain/linux_x86_glibc
 
-make \
-PYTHONHOME=${ZPYTHON_ROOT}:${ZPYTHON_ROOT}/Lib \
-HOSTPYTHON=./hostpython \
-HOSTPGEN=./Parser/hostpgen \
-PATH=${PATH}:${PLAT}"/bin" \
-CROSS_COMPILE="x86_64-nacl-" \
-CROSS_COMPILE_TARGET=yes \
-HOSTARCH=amd64-linux \
-BUILDARCH=x86_64-linux-gnu
+export PATH=${PATH}:${NACL_TOOLCHAIN}/bin:
 
+export PYTHONHOME=${ZPYTHON_ROOT}:${ZPYTHON_ROOT}/Lib
+export HOSTPYTHON=./hostpython
+export HOSTPGEN=./Parser/hostpgen
+export PATH=${PATH}:${PLAT}"/bin"
+export CROSS_COMPILE="x86_64-nacl-"
+export CROSS_COMPILE_TARGET=yes
+export HOSTARCH=amd64-linux
+export BUILDARCH=x86_64-linux-gnu
+
+make 
 
 #copy python files into _install directory, all installed files should be accessible
 #in filesystem in order to run python on zerovm+zrt
 
-#make install HOSTPYTHON=./hostpython CROSS_COMPILE=ppc_6xx- CROSS_COMPILE_TARGET=yes prefix=~/Python-3.2.2/_install
+#CROSS_COMPILE=ppc_6xx-
+
+make install HOSTPYTHON prefix=~/Python-3.2.2/_install
 
